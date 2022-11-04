675ae23cc0206379c328c53f6ad511829977d041
Sam Cassese <92000097+samcassese@users.noreply.github.com>
2022-11-04 17:07:10 +0000

    Added bones of the program with comments

--- Main.java
+++ Main.java
@@ -1,8 +1,63 @@
+/*
+
+- dollar amounts rounded to 2 decimal points
+- uses classes
+
+*/
+
+import java.util.*;
+
 class Main {
   
   public static void main(String[] args) {
+
+    // constants
+    int DAYS_IN_MONTH = 31;
+    //
     
-    System.out.println("Hello world");
+    System.out.println("This program asks for your monthly income and expenses, then tells you your net monthly income.");
+    System.out.println();
+
+    // ask # of categories of income
+
+
+    // ask each income amount
+
+
+    // asks if they input monthly or daily expenses
+    System.out.println("Enter 1) monthly or 2) daily expenses?");
+
+
+    // ask # of categories of income
+
+
+    // ask each expense amount
+
+
+
+    
+    /*
+/////////////////////////
+    User input done
+/////////////////////////
+    */
+
+
+
+    // prints total income (monthly and daily)
+
+
+
+    // prints total expenses (monthly and daily)
+
+
+
+    // prints how much they earned/lost
+
+    
+
+    // prints big saver/spender message
+    
     
   }
   
