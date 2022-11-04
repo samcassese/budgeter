dc9539e13fea2d60999df3ef39009f624795844e
Sam Cassese <92000097+samcassese@users.noreply.github.com>
2022-11-04 22:06:06 +0000

    Finished user input section

--- Main.java
+++ Main.java
@@ -12,9 +12,12 @@ class Main {
   
   public static void main(String[] args) {
 
-    int count = 0;
+    int incomeCount = 0;
+    int expensesCount = 0;
     double income = 0;
     double expenses = 0;
+
+    System.out.println();
     
     System.out.println("This program asks for your monthly income and expenses, then tells you your net monthly income.");
     System.out.println();
@@ -30,24 +33,37 @@ class Main {
     System.out.println();
 
     // ask each income amount
-    while(count < incomeCategories) {
+    while(incomeCount < incomeCategories) {
       System.out.println("Next income amount? ");
       income += scanner.nextDouble();
 
       System.out.println(income);
-      count++;
+      incomeCount++;
     }
 
+    System.out.println();
+    
     // asks if they input monthly or daily expenses
     System.out.println("Enter 1) monthly or 2) daily expenses?");
 
+    int monthlyOrDaily = scanner.nextInt();
 
-    // ask # of categories of income
 
+    // ask # of categories of expenses
+    System.out.print("How many categories of income? ");
+
+    int expenseCategories = scanner.nextInt();
+
+    System.out.println();
 
     // ask each expense amount
+    while(expensesCount < expenseCategories) {
+      System.out.println("Next expense amount? ");
+      expenses += scanner.nextDouble();
 
-
+      System.out.println(expenses);
+      expensesCount++;
+    }
 
     
     /*
@@ -59,7 +75,6 @@ class Main {
 
 
     // prints total income (monthly and daily)
-
 
 
     // prints total expenses (monthly and daily)
